ce7aa71e4df450244714ba3d7c1e4cb7c35fda2c
Juenemann-L-System <entwicklung@juenemann-instruments.de>
2026-06-24 14:16:31 +0200

    Refactor comments and echo statements in installer script

--- install_mqtt_forwarder.sh
+++ install_mqtt_forwarder.sh
@@ -7,7 +7,7 @@ echo "# ChirpStack MQTT Forwarder Installer #"
 echo "########################################"
 
 # ----------------------------
-# DEFAULT VALUES (deine letzten Werte)
+# DEFAULT VALUES
 # ----------------------------
 DEFAULT_GW_IP="192.168.2.1"
 DEFAULT_GW_USER="sysadmin"
@@ -15,7 +15,7 @@ DEFAULT_MQTT_IP="192.168.2.10"
 DEFAULT_MQTT_PORT="1883"
 
 # ----------------------------
-# INPUT (mit Defaults)
+# INPUT
 # ----------------------------
 read -p "Gateway IP [$DEFAULT_GW_IP]: " GW_IP
 GW_IP=${GW_IP:-$DEFAULT_GW_IP}
@@ -49,29 +49,27 @@ echo "Copying package to gateway..."
 sshpass -p "$GW_PASS" scp -o StrictHostKeyChecking=no /tmp/$PKG $GW_USER@$GW_IP:/tmp/
 
 # ----------------------------
-# INSTALL + CONFIGURE ON GATEWAY
+# INSTALL + CONFIGURE
 # ----------------------------
 echo "Installing and configuring on gateway..."
 
 sshpass -p "$GW_PASS" ssh -tt -o StrictHostKeyChecking=no $GW_USER@$GW_IP << EOF
 set -e
 
-echo "Installing package..."
+echo "Installing package (ignore if already installed)..."
 echo "$GW_PASS" | sudo -S opkg install /tmp/$PKG || true
-
-echo "Configuring AP1 MQTT..."
 
 CONF="/var/config/chirpstack-mqtt-forwarder/ap1/chirpstack-mqtt-forwarder.toml"
 
 if [ -f "\$CONF" ]; then
-    sed -i "s#^server=.*#server=\"tcp://$MQTT_IP:$MQTT_PORT\"#g" \$CONF
-    echo "MQTT configured: $MQTT_IP:$MQTT_PORT"
+    echo "Configuring MQTT..."
+    echo "$GW_PASS" | sudo -S sed -i "s#^server=.*#server=\"tcp://$MQTT_IP:$MQTT_PORT\"#g" "\$CONF"
 else
     echo "ERROR: Config file not found!"
     exit 1
 fi
 
-echo "Restarting service..."
+echo "Restarting forwarder..."
 echo "$GW_PASS" | sudo -S /etc/init.d/chirpstack-mqtt-forwarder-ap1 restart || true
 
 echo "DONE"
